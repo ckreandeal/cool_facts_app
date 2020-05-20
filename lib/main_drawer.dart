e955a5cdd8519dbede3dcff055f5bc111c45ec93
Ckrendel <danich1993@mail.ru>
2020-05-20 15:00:25 +0100

    Added facts model

--- lib/main_drawer.dart
+++ lib/main_drawer.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'models/fact_model.dart';
 
 class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Text('Hello world'),
-      
-      
+      child: Padding(
+        padding: const EdgeInsets.only(top: 35.0, left: 10.0),
+        child: Text(
+          'Hello world',
+          style: TextStyle(fontSize: 36.0),
+        ),
+      ),
     );
   }
 }
---
+++ lib/models/fact_model.dart
@@ -0,0 +1,40 @@
+class Fact {
+  final int id;
+  final String title;
+  final String text;
+  final String picturePath;
+
+  Fact({
+    this.id,
+    this.title,
+    this.text,
+    this.picturePath,
+  });
+}
+
+final List<Fact> facts = [
+  Fact(
+    id: 0, 
+    title: 'Superman didn\'t always fly.', 
+    text: 'The original comic book Superman could leap tall buildings in a single bound. But then he had to come right back down to Earth—because he didn\'t fly. It wasn\'t until the 1940s, when animators for a new animated series decided it would be too difficult to routinely draw him bending his knees, that it was decided that Superman could take off into the air. Readers got to see smooth animation, and a superhero gained a new power.', 
+    picturePath: 'images/superman.jpeg'),
+
+  Fact(
+    id: 1, 
+    title: 'The first computer was invented in the 1940s.', 
+    text: 'The original comic book Superman could leap tall buildings in a single bound. But then he had to come right back down to Earth—because he didn\'t fly. It wasn\'t until the 1940s, when animators for a new animated series decided it would be too difficult to routinely draw him bending his knees, that it was decided that Superman could take off into the air. Readers got to see smooth animation, and a superhero gained a new power.', 
+    picturePath: 'images/superman.jpeg'),
+
+  Fact(
+    id: 2, 
+    title: 'Space smells like seared steak.', 
+    text: 'The original comic book Superman could leap tall buildings in a single bound. But then he had to come right back down to Earth—because he didn\'t fly. It wasn\'t until the 1940s, when animators for a new animated series decided it would be too difficult to routinely draw him bending his knees, that it was decided that Superman could take off into the air. Readers got to see smooth animation, and a superhero gained a new power.', 
+    picturePath: 'images/superman.jpeg'),
+
+  Fact(
+    id: 3, 
+    title: 'The longest wedding veil was the same length as 63.5 football fields.', 
+    text: 'The original comic book Superman could leap tall buildings in a single bound. But then he had to come right back down to Earth—because he didn\'t fly. It wasn\'t until the 1940s, when animators for a new animated series decided it would be too difficult to routinely draw him bending his knees, that it was decided that Superman could take off into the air. Readers got to see smooth animation, and a superhero gained a new power.', 
+    picturePath: 'images/superman.jpeg'), 
+
+];
